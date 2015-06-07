06c55b3f56a0ea8ca6bf3871c06cf0241f08b4a9
Niklas Sletteland <niklassletteland@gmail.com>
2015-06-07 23:09:14 +0500

    remove grunt ftp task, switching to git-based deploy mechanism

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,23 +11,13 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
-    },
-    git_ftp: {
-      default: {
-        options: {
-          'hostFile':'.gitftppass',
-          'host':'default'
-        }
-      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   //grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-git-ftp');
 
   // Default task(s).
   //grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('deploy', ['git_ftp']);
 
 };
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Repo for updated personal website, still a work in progress.
+
+To deploy to digitalocean:
+
+    git push dev master
+    git push stage master
+    git push live master
